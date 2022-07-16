10b328061be933ac11e5d728c01ecb88b50b7737
thyoon515 <thyoon515@gmail.com>
2022-07-15 23:38:30 -0400

    Add components

---
+++ src/components/Recipes/FavoriteRecipe.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FavoriteRecipe = () => {
+  return (
+    <div>FavoriteRecipe</div>
+  );
+}
+
+export default FavoriteRecipe;
---
+++ src/components/Recipes/IngredientsRecipe.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const IngredientsRecipe = () => {
+  return (
+    <div>IngredientsRecipe</div>
+  );
+}
+
+export default IngredientsRecipe;
